3ca86f246502bc3bcaec15f247bfe0e208deeefb
Sebastian Müller <mueller.seb@posteo.de>
2024-04-25 16:50:54 +0200

    create version dynamically based on git tags

--- src/cell_tree2d/__init__.py
+++ src/cell_tree2d/__init__.py
@@ -1,8 +1,12 @@
-#!/usr/bin/env python
-
-from __future__ import absolute_import
+"""Python wrappers around Cell-Tree 2D spatial index."""
 
 # Just bring in the CellTree into this namespace.
 from .cell_tree2d import CellTree
 
-__version__ = "0.3.0"
+try:
+    from ._version import __version__
+except ImportError:  # pragma: nocover
+    # package is not installed
+    __version__ = "0.0.0.dev0"
+
+__all__ = ["__version__", "CellTree"]
